1252bfc646fcc066e02674415df7ba18b0c275d7
hagen-gloetter <38193521+hagen-gloetter@users.noreply.github.com>
2022-09-28 22:53:08 +0200

    changed to classes - working

--- code/test.py
+++ code/test.py
@@ -7,19 +7,55 @@ from machine import Pin
 from time import sleep_ms
 
 from web_html import web_page, web_css
-from debounce_class import Button
-from rotary_encoder import get_rotary_encoder
+from debounce_class import Button, button_a_callback, button_b_callback
+#from rotary_encoder import get_rotary_encoder
+#from rotary_encoder import get_rotary_encoder
+#from rotary_irq_esp import RotaryIRQ
+#from rotary_encoder_class import Rotary
+from rotary_encoder_class_2 import RotaryIRQ
 
 webpage=web_page()
 #print (webpage)
 webcss=web_css()
 #print (webcss)
 led = machine.Pin(2, machine.Pin.OUT)
+#r = Rotary(pin_num_clk=33,
+#    pin_num_dt=34,
+#    min_val=1,
+#    max_val=255,
+#    reverse=True,
+#    range_mode=RotaryIRQ.RANGE_WRAP)
+r = RotaryIRQ(
+    pin_num_clk=33,
+    pin_num_dt=34,
+    min_val=1,
+    max_val=255,
+    reverse=True,
+    range_mode=RotaryIRQ.RANGE_WRAP)
+sw = Pin(14, Pin.IN)
+val_old = r.value()
+isRotaryEncoder = True
+
+BUTTON_A_PIN = const(10)
+BUTTON_B_PIN = const(13)
+button_a = Button(pin=Pin(BUTTON_A_PIN, mode=Pin.IN, pull=Pin.PULL_UP), trigger=Pin.IRQ_RISING, callback=button_a_callback)
+button_b = Button(pin=Pin(BUTTON_B_PIN, mode=Pin.IN, pull=Pin.PULL_UP), trigger=Pin.IRQ_RISING, callback=button_b_callback)
+
 while True:
-    led.value(1)
+#    led.value(1)
+#    sleep_ms(200)
+#    led.value(0)
+    
+    r_value = r.get_rotary_encoder(sw, val_old)
+    if r_value != None:
+        print(r_value)
+        val_old = r_value
+    
     sleep_ms(200)
-    led.value(0)
-    sleep_ms(200)
-    get_rotary_encoder()
+#    if isRotaryEncoder == True:
+#        val_new = r.value()
+#        if val_old != val_new:
+#            val_old = val_new
+#            print('result = {}'.format(val_new))
 
     
