0d891419c3059b94b577f61ed7a57676e6b42cad
hagen-gloetter <38193521+hagen-gloetter@users.noreply.github.com>
2022-09-28 22:53:08 +0200

    changed to classes - working

--- code/debounce_class.py
+++ code/debounce_class.py
@@ -2,12 +2,14 @@ import time
 from micropython import const
 from machine import Pin
 
-BUTTON_A_PIN = const(13)
-BUTTON_B_PIN = const(10)
+BUTTON_A_PIN = const(10)
+BUTTON_B_PIN = const(13)
 Button_A_Status = "OFF"
 Button_A_cnt = 0
 Button_B_Status = "OFF"
 Button_B_cnt = 0
+Light_A = "OFF" # white
+Light_B = "OFF" # red
 
 print('Debounce Class loaded')
 
@@ -24,6 +26,31 @@ class Button:
         if time.ticks_ms() > self._next_call:
             self._next_call = time.ticks_ms() + self.min_ago
             self.call_callback(pin)
+
+def button_a_switch():
+    global Light_A
+    global Light_B
+    if Light_A == "OFF" and Light_B == "OFF":
+        Light_A = "ON"
+        print("White ON")
+    elif Light_A == "ON" and Light_B == "OFF":
+        Light_A = "OFF"
+        print("White OFF")
+
+def button_b_switch():
+    global Light_A
+    global Light_B
+    if Light_B == "OFF" and Light_A == "OFF":
+        Light_B = "ON"
+        print("Red ON")
+    elif Light_B == "OFF" and Light_A == "ON":
+        Light_A = "OFF"
+        Light_B = "ON"
+        print("Red over White ON")
+    else:
+        Light_B = "OFF"
+        print("Red OFF")
+
 def button_a_callback(pin):
     global Button_A_cnt
     global Button_A_Status
@@ -35,6 +62,7 @@ def button_a_callback(pin):
             else:
                 Button_A_Status = "ON"
             print(f"Button A (%s) cnt:{Button_A_cnt} status:{Button_A_Status}" % pin)
+            button_a_switch()
         else:
             pass
         Button_A_cnt +=1
@@ -51,6 +79,7 @@ def button_b_callback(pin):
             else:
                 Button_B_Status = "ON"
             print(f"Button B (%s) cnt:{Button_B_cnt} status:{Button_B_Status}" % pin)
+            button_b_switch()
         else:
             pass
         Button_B_cnt +=1
---
+++ code/rotary_encoder_class_2.py
@@ -0,0 +1,104 @@
+# The MIT License (MIT)
+# Copyright (c) 2020 Mike Teachman
+# https://opensource.org/licenses/MIT
+
+# Platform-specific MicroPython code for the rotary encoder module
+# ESP8266/ESP32 implementation
+
+# Documentation:
+#   https://github.com/MikeTeachman/micropython-rotary
+
+from machine import Pin
+from rotary import Rotary
+from sys import platform
+from time import sleep_ms
+
+class RotaryIRQ(Rotary):
+
+    def __init__(self, pin_num_clk, pin_num_dt, min_val=0, max_val=10, reverse=False, range_mode=Rotary.RANGE_UNBOUNDED, pull_up=False, half_step=False, invert=False):
+
+        super().__init__(min_val, max_val, reverse, range_mode, half_step, invert)
+
+        if pull_up == True:
+            self._pin_clk = Pin(pin_num_clk, Pin.IN, Pin.PULL_UP)
+            self._pin_dt = Pin(pin_num_dt, Pin.IN, Pin.PULL_UP)
+        else:
+            self._pin_clk = Pin(pin_num_clk, Pin.IN)
+            self._pin_dt = Pin(pin_num_dt, Pin.IN)
+
+        self._enable_clk_irq(self._process_rotary_pins)
+        self._enable_dt_irq(self._process_rotary_pins)
+
+    def _enable_clk_irq(self, callback=None):
+        self._pin_clk.irq(
+            trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
+            handler=callback)
+
+    def _enable_dt_irq(self, callback=None):
+        self._pin_dt.irq(
+            trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
+            handler=callback)
+
+    def _disable_clk_irq(self):
+        self._pin_clk.irq(handler=None)
+
+    def _disable_dt_irq(self):
+        self._pin_dt.irq(handler=None)
+
+    def _hal_get_clk_value(self):
+        return self._pin_clk.value()
+
+    def _hal_get_dt_value(self):
+        return self._pin_dt.value()
+
+    def _hal_enable_irq(self):
+        self._enable_clk_irq(self._process_rotary_pins)
+        self._enable_dt_irq(self._process_rotary_pins)
+
+    def _hal_disable_irq(self):
+        self._disable_clk_irq()
+        self._disable_dt_irq()
+
+    def _hal_close(self):
+        self._hal_disable_irq()
+        
+    def get_rotary_encoder(self, sw, val_old):
+        global isRotaryEncoder
+#        global sw
+#        if sw.value() == 1:
+#            isRotaryEncoder = not isRotaryEncoder
+#            if isRotaryEncoder == True:
+#                print('Rotary Encoder is now enabled.')
+#            else:
+#                print('Rotary Encoder is now disabled.')
+        if sw.value() == 0:
+            isRotaryEncoder = not isRotaryEncoder
+            if isRotaryEncoder == True:
+                print('Rotary Encoder is now enabled.')
+            else:
+                print('Rotary Encoder is now disabled.')
+        if isRotaryEncoder == True:
+#            global val_old
+            val_new = self.value()
+            if val_old != val_new:
+                val_old = val_new
+                print('result = {}'.format(val_new))
+                return val_new
+        
+#r = RotaryIRQ(
+#    pin_num_clk=33,
+#    pin_num_dt=34,
+#    min_val=1,
+#    max_val=255,
+#    reverse=True,
+#    range_mode=RotaryIRQ.RANGE_WRAP)
+
+#sw = Pin(14, Pin.IN)
+#sw_val_old = sw.value()
+#val_old = r.value()
+
+isRotaryEncoder = True
+
+#while True:
+#    r.get_rotary_encoder()
+#    sleep_ms(200)
