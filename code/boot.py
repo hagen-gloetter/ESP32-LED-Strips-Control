3faec368d586722027332f418d9e1c4f18ca4d3b
hagen-gloetter <38193521+hagen-gloetter@users.noreply.github.com>
2022-10-01 02:07:15 +0200

    Debugging of boot-fails - no Wifi

--- code/boot.py
+++ code/boot.py
@@ -1,17 +1,27 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
+import esp
 #esp.osdebug(None)
-#import webrepl
-#webrepl.start()
-
 import ujson
+from machine import Pin
+import network
+import esp
+#esp.osdebug(None)
+import gc
+import time
+import network
+try:
+    import usocket as socket
+except:
+    import socket
+import webrepl
+print ("boot.py") 
 
 ssid = ""
 password = ""
 
 def get_wlankeys():
 # load WLAN Secrets
-    with open("secrets.json") as fp:
+    with open("secretsSW.json") as fp:
         secrets = ujson.load(fp)
     global ssid
     global password
@@ -23,59 +33,18 @@ def get_wlankeys():
 
 def do_connect(ssid, pwd):
     import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
+    station = network.WLAN(network.STA_IF)
+    if not station.isconnected():
         print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(ssid, pwd)
-        while not sta_if.isconnected():
+        station.active(True)
+        station.connect(ssid, pwd)
+        while not station.isconnected():
+            sleep_ms(10)
             pass
-    print('network config:', sta_if.ifconfig())
- 
-# This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
- 
-# Attempt to connect to WiFi network
+    print('network config:', station.ifconfig())
 
-get_wlankeys()
-print (ssid)
-print (password)
-
-do_connect(ssid, password)
- 
-import webrepl
-webrepl.start()
-
-try:
-  import usocket as socket
-except:
-  import socket
-
-from machine import Pin
-import network
-
-import esp
-esp.osdebug(None)
-
-import gc
-gc.collect()
-
-import time
-last_on = 0
-
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected() == False:
-  pass
-
-print('Connection successful')
-print(station.ifconfig())
-
-lights_on = 0
-
-led = Pin(2, Pin.OUT)
-
+#webrepl.start()
+#get_wlankeys()
+#print (ssid)
+#print ("pw")
+#do_connect(ssid, password)
