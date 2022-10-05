55a568c0719f890d7b2fc06ca53fd13902f7d09b
hagen-gloetter <38193521+hagen-gloetter@users.noreply.github.com>
2022-10-06 00:20:12 +0200

    Fade LEDs in an out

--- code/main.py
+++ code/main.py
@@ -17,14 +17,15 @@ print("main.py")
 #from web_html import web_css
 
 led = machine.Pin(2, machine.Pin.OUT)
-Hue = 256
-ColorR = 0
-ColorG = 0
-ColorB = 0
-Strip1 = LEDStrip(ColorR, ColorG, ColorB, 27, 25, 32)
-Strip1.SetColor(0, 0, 0)
-Strip2 = LEDStrip(ColorR, ColorG, ColorB, 17, 21, 22)
-Strip2.SetColor(0, 0, 0)
+Brightness = 256
+
+ColorRGB=[0,0,0]
+ColorSollwerte = [0,0,0]
+
+Strip1 = LEDStrip(ColorRGB[0], ColorRGB[1], ColorRGB[2], 27, 25, 32)    # TODO  Listen übergeben
+Strip1.SetColor(ColorRGB[0], ColorRGB[1], ColorRGB[2])
+Strip2 = LEDStrip(ColorRGB[0], ColorRGB[1], ColorRGB[2], 17, 21, 22)
+Strip2.SetColor(ColorRGB[0], ColorRGB[1], ColorRGB[2])
 
 r = RotaryIRQ(pin_num_clk=33, pin_num_dt=34, min_val=0, max_val=16,
               reverse=True, range_mode=RotaryIRQ.RANGE_WRAP)
@@ -44,114 +45,131 @@ led.value(1)
 sleep_ms(200)
 led.value(0)
 
-def strips_update_hue():
-    if Light_A == "ON":
-        Strip1.SetColor(Hue, Hue, Hue)
-        Strip2.SetColor(Hue, Hue, Hue)
-        print(f"Hue White Changed: {Hue}")
-    elif Light_B == "ON":
-        Strip1.SetColor(Hue, 0, 0)
-        Strip2.SetColor(Hue, 0, 0)
-        print(f"Hue Red Changed: {Hue}")
-    set_JSON(Light_A,Light_B)
+def strips_update_Brightness():
+    global ColorSollwerte
+    global Brightness
+    if Light_W == "ON":
+        ColorSollwerte=[Brightness,Brightness,Brightness] # set Brightness to Sollwerte
+        print(f"Brightness White Changed: {Brightness}")
+    elif Light_R == "ON":
+        ColorSollwerte=[Brightness,0,0]
+        print(f"Brightness Red Changed: {Brightness}") # set Brightness to Sollwerte
+
+    for i in range(ColorRGB):
+        if ColorRGB[i] > ColorSollwerte[i]: # fade in
+            ColorRGB[i] += 1
+        elif ColorRGB[i] < ColorSollwerte[i] # fade out
+            ColorRGB[i] -= 1
+        else:
+            pass # values are equal
+    Strip1.SetColor(ColorRGB[0], ColorRGB[2], ColorRGB[2]) # TODO: LIST as Parameter
+    Strip2.SetColor(ColorRGB[0], ColorRGB[2], ColorRGB[2])
+    set_JSON(Light_W,Light_R)
         
 def set_JSON(status1, status2):
     global JSONdata
-    global Hue
-    s1='{\n"button_red":"'  +  str(Light_B) + '",\n'
-    s2='"button_white":"'   +  str(Light_A) + '",\n'
-#    s3='"LED_brightness":"' +  " R="+ str(ColorR) + " G="+ str(ColorG) + " B="+ str(ColorB) +'",\n'
-    s3='"LED_brightness":"' +  str(Hue) + '",\n'
+    global Brightness
+    s1='{\n"button_red":"'  +  str(Light_R) + '",\n'
+    s2='"button_white":"'   +  str(Light_W) + '",\n'
+    s3='"LED_brightness":"' +  str(Brightness) + '",\n'
     s4='"LED_roof":"'       +  "NA" + '"\n}\n'
     JSONdata=s1+s2+s3+s4
 
-def button_a_switch():
-    global Light_A
-    global Light_B
-    if Light_A == "OFF" and Light_B == "OFF":
-        Light_A = "ON"
+def Button_W_switch():
+    global Light_W
+    global Light_R
+    global ColorSollwerte
+    global Brightness
+    if Light_W == "OFF" and Light_R == "OFF":
+        Light_W = "ON"
         print("White ON")
-        Strip1.SetColor(Hue, Hue, Hue)
-        Strip2.SetColor(Hue, Hue, Hue)
-    elif Light_A == "ON" and Light_B == "OFF":
-        Light_A = "OFF"
+#        Strip1.SetColor(Brightness, Brightness, Brightness)
+#        Strip2.SetColor(Brightness, Brightness, Brightness)
+        ColorSollwerte=[Brightness,Brightness,Brightness]
+    elif Light_W == "ON" and Light_R == "OFF":
+        Light_W = "OFF"
         print("White OFF")
-        Strip1.SetColor(0, 0, 0)
-        Strip2.SetColor(0, 0, 0)
-    set_JSON(Light_A,Light_B)
+#        Strip1.SetColor(0, 0, 0)
+#        Strip2.SetColor(0, 0, 0)
+#        ColorSollwerte=[0,0,0]
+        ColorSollwerte=[0,0,0]
+    set_JSON(Light_W,Light_R)
 
-def button_b_switch():
-    global Light_A
-    global Light_B
-    if Light_B == "OFF" and Light_A == "OFF":
-        Light_B = "ON"
+def Button_R_switch():
+    global Light_W
+    global Light_R
+    global ColorSollwerte
+    if Light_R == "OFF" and Light_W == "OFF":
+        Light_R = "ON"
         print("Red ON")
-        Strip1.SetColor(Hue, 0, 0)
-        Strip2.SetColor(Hue, 0, 0)
-    elif Light_B == "OFF" and Light_A == "ON":
-        Light_A = "OFF"
-        Light_B = "ON"
+#        Strip1.SetColor(Brightness, 0, 0)
+#        Strip2.SetColor(Brightness, 0, 0)
+        ColorSollwerte=[Brightness,0,0]
+    elif Light_R == "OFF" and Light_W == "ON":
+        Light_W = "OFF"
+        Light_R = "ON"
         print("Red over White ON")
-        Strip1.SetColor(Hue, 0, 0)
-        Strip2.SetColor(Hue, 0, 0)
+#        Strip1.SetColor(Brightness, 0, 0)
+#        Strip2.SetColor(Brightness, 0, 0)
+        ColorSollwerte=[0,0,0]      
     else:
-        Light_B = "OFF"
+        Light_R = "OFF"
         print("Red OFF")
-        Strip1.SetColor(0, 0, 0)
-        Strip2.SetColor(0, 0, 0)
-    set_JSON(Light_A,Light_B)
+ #       Strip1.SetColor(0, 0, 0)
+ #       Strip2.SetColor(0, 0, 0)
+        ColorSollwerte=[0,0,0]      
+    set_JSON(Light_W,Light_R)
 
-
-def button_a_callback(pin):
-    global Button_A_cnt
-    global Button_A_Status
+def Button_W_callback(pin):
+    global Button_W_cnt
+    global Button_W_Status
     if pin.value() == 1:
-        if Button_A_cnt % 2 == 0:
-            Button_A_cnt = 0
-            if Button_A_Status == "ON":
-                Button_A_Status = "OFF"
+        if Button_W_cnt % 2 == 0:
+            Button_W_cnt = 0
+            if Button_W_Status == "ON":
+                Button_W_Status = "OFF"
             else:
-                Button_A_Status = "ON"
+                Button_W_Status = "ON"
             print(
-                f"Button A (%s) cnt:{Button_A_cnt} status:{Button_A_Status}" % pin)
-            button_a_switch()
+                f"Button A (%s) cnt:{Button_W_cnt} status:{Button_W_Status}" % pin)
+            Button_W_switch()
         else:
             pass
-        Button_A_cnt += 1
+        Button_W_cnt += 1
 
 
-def button_b_callback(pin):
-    global Button_B_cnt
-    global Button_B_Status
+def Button_R_callback(pin):
+    global Button_R_cnt
+    global Button_R_Status
     if pin.value() == 1:
-        Button_B_cnt %= 2
-        if Button_B_cnt == 1:
-            #            Button_B_cnt = 0
-            if Button_B_Status == "ON":
-                Button_B_Status = "OFF"
+        Button_R_cnt %= 2
+        if Button_R_cnt == 1:
+            #            Button_R_cnt = 0
+            if Button_R_Status == "ON":
+                Button_R_Status = "OFF"
             else:
-                Button_B_Status = "ON"
+                Button_R_Status = "ON"
             print(
-                f"Button B (%s) cnt:{Button_B_cnt} status:{Button_B_Status}" % pin)
-            button_b_switch()
+                f"Button B (%s) cnt:{Button_R_cnt} status:{Button_R_Status}" % pin)
+            Button_R_switch()
         else:
             pass
-        Button_B_cnt += 1
+        Button_R_cnt += 1
 
 
-BUTTON_A_PIN = const(13)
-BUTTON_B_PIN = const(10)
-Button_A_Status = "OFF"
-Button_A_cnt = 0
-Button_B_Status = "OFF"
-Button_B_cnt = 0
-Light_A = "OFF"  # white
-Light_B = "OFF"  # red
+Button_W_PIN = const(13)
+Button_R_PIN = const(10)
+Button_W_Status = "OFF"
+Button_W_cnt = 0
+Button_R_Status = "OFF"
+Button_R_cnt = 0
+Light_W = "OFF"  # white
+Light_R = "OFF"  # red
 
-button_a = Button(pin=Pin(BUTTON_A_PIN, mode=Pin.IN, pull=Pin.PULL_UP),
-                  trigger=Pin.IRQ_RISING, callback=button_a_callback)
-button_b = Button(pin=Pin(BUTTON_B_PIN, mode=Pin.IN, pull=Pin.PULL_UP),
-                  trigger=Pin.IRQ_RISING, callback=button_b_callback)
+Button_W = Button(pin=Pin(Button_W_PIN, mode=Pin.IN, pull=Pin.PULL_UP),
+                  trigger=Pin.IRQ_RISING, callback=Button_W_callback)
+Button_R = Button(pin=Pin(Button_R_PIN, mode=Pin.IN, pull=Pin.PULL_UP),
+                  trigger=Pin.IRQ_RISING, callback=Button_R_callback)
 
 # button part end =========================================================================================================
 
@@ -210,8 +228,8 @@ except:
 
 Strip1.SetColor(0,0,0)
 Strip2.SetColor(0,0,0)
-strips_update_hue()
-set_JSON(Light_A,Light_B)
+strips_update_Brightness()
+set_JSON(Light_W,Light_R)
 
 #_thread.start_new_thread(thread_webserver,( 4, "webserver"))
 #debug no thread
@@ -223,23 +241,23 @@ while True:
     led.value(toggle)
     #    sleep_ms(200)
     #    led.value(0)
+    if Light_W=="ON" or Light_R=="ON": # only if at least one switch is on
+        r_value = r.get_rotary_encoder(sw, val_old)
+        if r_value != None:
+            if val_old == 16 and r_value == 0:
+                r_value = 16  # fix overflow
+            if val_old == 0 and r_value == 16:
+                r_value = 0
+            val_old = r_value
 
-    r_value = r.get_rotary_encoder(sw, val_old)
-    if r_value != None:
-        if val_old == 16 and r_value == 0:
-            r_value = 16  # fix overflow
-        if val_old == 0 and r_value == 16:
-            r_value = 0
-        val_old = r_value
-
-        Hue = r_value*16  # tranlate 16 steps to 255 color-steps
-        if Hue > 256:
-            Hue = 255
-        if Hue < 1:
-            Hue = 0
-        print(f"Hue={Hue}")
-        strips_update_hue()
-    sleep_ms(200)
+        Brightness = r_value*16  # tranlate 16 steps to 255 color-steps and set limits
+        if Brightness > 256:
+            Brightness = 255
+        if Brightness < 1:
+            Brightness = 0
+        print(f"Brightness update ={Brightness}")
+    strips_update_Brightness()
+    sleep_ms(20)
     cnt +=1
     if cnt == 5:
         print ("main loop")
